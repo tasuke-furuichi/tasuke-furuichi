19507da4ac162f1aaa7e26540506e5da5af0c777
TASUKE <tasuke_0821@yahoo.co.jp>
2017-07-13 16:48:06 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,18 +8,46 @@ class MessagesController < ApplicationController
   end
   
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success]='Messageが正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger]='Messageが投稿されませんでした'
+      render :new
+    end
   end
   
   def new
+    @message = Message.new
   end
   
   def update
+    @message = Message.find(params[:id])
+    if @message.save
+      flash[:success]='Messageを正常に更新しました'
+      redirect_to @message
+    else
+      flash.now[:danger]="Messageを更新できませんでした"
+      render :edit
+    end
   end
   
   def edit
+    @message = Message.find(params[:id])
   end
   
   def destroy
+    @message =Message.find(params[:id])
+    @message.destroy
+    
+    flash[:success]='Messageは正常に削除されました'
+    redirect_to message_url
   end
-  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713100059) do
+ActiveRecord::Schema.define(version: 20170713125138) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
